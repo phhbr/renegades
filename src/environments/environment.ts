3dd00f967240646b9630da6179f0da6fdaf36a3d
phhbr <phhbr@phhbr.de>
2025-01-23 18:09:45 +0100

    chore: test keys for local deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     url: 'https://ftgcbmthbwwcumvqnuof.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0Z2NibXRoYnd3Y3VtdnFudW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NDQ3MjcsImV4cCI6MjA1MjUyMDcyN30.sfmgAXCpMDpizmM7_kFNX3bSns_Awcl_jRJEe-kwoWk' },
   recaptcha: {
-    siteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY ?? '6LfTgr8qAAAAAB6vUF6Y8w5DZSuINMy1uYMYa0TB'
+    siteKey: '6LfTgr8qAAAAAB6vUF6Y8w5DZSuINMy1uYMYa0TB'
   }
 };
