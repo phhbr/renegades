b0b97f1b096b048b5306b1c87a3ff6e7bcd0c265
phhbr <phhbr@phhbr.de>
2025-01-28 21:29:41 +0100

    chore: added missing translations

--- src/app/i18n/de/team.ts
+++ src/app/i18n/de/team.ts
@@ -2,6 +2,7 @@ export const team = {
   'team.title': 'Unser Team',
   'team.staff.title': 'Trainer-Team',
   'team.players.title': 'Spieler',
+  'team.role.ceo': 'Vorstandsvorsitzender',
   'team.role.head_coach': 'Head Coach',
   'team.role.offensive_coordinator': 'Offensive Coordinator',
   'team.role.defensive_coordinator': 'Defensive Coordinator',
