03741c817f0082a743f3e020213c3d8eebbaf885
phhbr <phhbr@phhbr.de>
2025-11-08 14:31:17 +0100

    feat: restructure training section (location)

--- src/app/components/training/training.component.ts
+++ src/app/components/training/training.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { TryoutFormComponent } from './tryout-form.component';
 import { TranslatePipe } from '../../pipes/translate.pipe';
@@ -11,6 +11,14 @@ import { CookieConsentService } from '../../services/cookie-consent.service';
   templateUrl: './training.component.html'
 })
 export class TrainingComponent {
+  private currentMonth = signal(new Date().getMonth() + 1); // 1-12
+
+  isOffSeason = computed(() => {
+    // Off-season is November (11) through March (3)
+    const month = this.currentMonth();
+    return month >= 11 || month <= 3;
+  });
+
   constructor(private cookieConsentService: CookieConsentService) {}
 
   showMap(): boolean {
