48df34effc71992d4817489a5044c70a33838ca4
phhbr <phhbr@phhbr.de>
2026-08-06 13:52:36 +0200

    fix: update footer translations for consistency and adjust navbar styles for responsiveness

--- src/app/i18n/de/footer.ts
+++ src/app/i18n/de/footer.ts
@@ -3,12 +3,11 @@ export const footer = {
   'footer.impressum': 'Impressum',
   'footer.privacy': 'Datenschutzerklärung',
   'footer.rights': 'Alle Rechte vorbehalten.',
-  'footer.license.source': 'Der Quellcode ist unter der',
+  'footer.license.source': 'Der Quellcode ist unter der ',
   'footer.license.mit': 'MIT-Lizenz',
-  'footer.license.licensed': 'lizenziert.',
+  'footer.license.licensed': ' lizenziert.',
   'footer.images.credits': 'Bildnachweise',
-  'footer.images.background': 'Hintergrundbilder: Hero-Bild von',
-  'footer.images.team': 'Team-Bild von',
-  'footer.images.contact': 'Kontakt-Bild von',
+  'footer.images.background': 'Hintergrundbilder: Hero-Bild von ',
+  'footer.images.team': 'Team-Bild von ',
   'footer.images.club': 'Team-Fotos und Club-Logo © {{ year }} Nürnberg Renegades e.V. Alle Rechte vorbehalten.'
 };
